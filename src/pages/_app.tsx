01dbef18c4ba3b020a0fd99137e38f7ce59cd952
felipe-AGomes <falmeidagomes13@gmail.com>
2023-04-09 09:00:27 -0300

    Remove mysql database

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
-import connectDb from '@/services/connectDBMong';
-// connectDb();
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
