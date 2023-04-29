ec1d60880bdbf293fbdde1a2564f48db239b1278
felipe-AGomes <falmeidagomes13@gmail.com>
2023-04-29 10:59:08 -0300

    Login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,26 @@
 import { signIn } from 'next-auth/react';
 import style from './login.module.css';
 import { useRouter } from 'next/router';
+import { Button } from '@chakra-ui/react';
 
 export default function Login() {
 	return (
 		<main id={style.login}>
 			<div className={style.modal}>
-				<button onClick={() => signIn('', { callbackUrl: '/home' })}>Login</button>
+				<h1
+					style={{
+						fontWeight: '500',
+						fontSize: '1.2rem',
+					}}
+				>
+					Você não está logado
+				</h1>
+				<Button
+				colorScheme='blue'
+				fontWeight={'bold'}
+				width={150}
+				height={50}
+				onClick={() => signIn('', { callbackUrl: '/home' })}>Login</Button>
 			</div>
 		</main>
 	);
