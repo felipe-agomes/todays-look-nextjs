eeef1fd25534d4bc0797ee01bcdcaad6c62cc9cf
felipe-AGomes <falmeidagomes13@gmail.com>
2023-05-10 22:43:27 -0300

    included spinner on load, into ModalDelete

--- src/components/ModalDelete/index.tsx
+++ src/components/ModalDelete/index.tsx
@@ -1,13 +1,14 @@
 import { OpenOrCloseModalProps } from '@/@types';
 import Style from './ModalDelete.module.css';
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Button, ButtonGroup, Spinner } from '@chakra-ui/react';
 import deleteSet from '@/pages/api/protected/user/[userId]/clothe/deleteSet/[setId]';
+import { useState } from 'react';
 
 type Props = {
 	openOrCloseModal: (
 		{ whichModal, operation }: OpenOrCloseModalProps,
 		clotheId?: string | null,
-		setId?: string | null
+		setId?: string | null,
 	) => void;
 	deleteClothe?: () => Promise<void>;
 	deleteSet?: () => Promise<void>;
@@ -18,8 +19,16 @@ export default function ModalDelete({
 	deleteClothe,
 	deleteSet,
 }: Props) {
+	const [loading, setLoading] = useState<boolean>(false);
+
 	return (
 		<div className={Style.modalContainer}>
+			{loading && (
+				<Spinner
+					color={'cyan'}
+					className={Style.spinner}
+				/>
+			)}
 			<h1>Deseja realmente remover a roupa?</h1>
 			<ButtonGroup gap={4}>
 				<Button
@@ -34,9 +43,11 @@ export default function ModalDelete({
 					color={'white'}
 					colorScheme={'red'}
 					onClick={async () => {
+						setLoading(true);
 						deleteClothe && (await deleteClothe());
 						deleteSet && (await deleteSet());
 						openOrCloseModal({ whichModal: 'setModal', operation: 'close' });
+						setLoading(false);
 					}}
 				>
 					Sim
