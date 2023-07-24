eaab349f0799417fc7a5916059d07120770a2aed
Felipe <falmeidagomes13@gmail.com>
2023-07-24 14:08:59 -0300

    refactor: delete unnecessary function

--- src/functions/uniqueCategories.ts
+++
@@ -1,6 +0,0 @@
-export default function uniqueCategories(categories: string[]) {
-	const newCategories = ['Favoritos', 'Todos', ...categories];
-	return newCategories.filter(
-		(category, index) => newCategories.indexOf(category) === index
-	);
-}
