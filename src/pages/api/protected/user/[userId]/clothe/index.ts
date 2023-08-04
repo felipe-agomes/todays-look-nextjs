0a12084a72cc97eb2256325def772f6d440fcbeb
Felipe <falmeidagomes13@gmail.com>
2023-08-04 15:00:20 -0300

    create new api rote

---
+++ src/pages/api/protected/user/[userId]/clothe/index.ts
@@ -0,0 +1,29 @@
+import { clotheModelMongo } from '@/models/ClotheModelMongo';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	if (req.method !== 'GET') {
+		res.status(400).json({
+			status: 'error',
+			message: 'Metodo não permitido',
+		});
+		return;
+	}
+	const userId = req.query.userId as string;
+	try {
+		const clothes = await clotheModelMongo.getAllByUserId({ userId });
+		res.status(200).json({
+			status: 'success',
+			message: 'Roupas buscadas com sucesso',
+			data: clothes,
+		});
+	} catch {
+		res.status(400).json({
+			status: 'error',
+			message: 'Erro ao buscar roupas',
+		});
+	}
+}
