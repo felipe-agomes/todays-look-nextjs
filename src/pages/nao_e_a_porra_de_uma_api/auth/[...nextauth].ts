da13cfb56ef036f25d4c6f696ffbfaf126dd6de9
Felipe <falmeidagomes13@gmail.com>
2023-08-15 23:12:51 -0300

    try fix deploy issue

--- src/pages/nao_e_a_porra_de_uma_api/auth/[...nextauth].ts
+++ src/pages/nao_e_a_porra_de_uma_api/auth/[...nextauth].ts
@@ -8,13 +8,6 @@ export const authOptions: AuthOptions = {
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID,
 			clientSecret: process.env.GOOGLE_SECRET,
-			authorization: {
-				params: {
-					prompt: 'consent',
-					access_type: 'offline',
-					response_type: 'code',
-				},
-			},
 		}),
 	],
 	secret: process.env.JWT_SECRET,
