a2c6856bf11d27061f9fa7ed1fd4c4fb630b1060
Felipe <falmeidagomes13@gmail.com>
2023-08-19 11:34:41 -0300

    created atuhentication on clothes api

--- src/pages/api/protected/user/[userId]/clothe/[clotheId].ts
+++ src/pages/api/protected/user/[userId]/clothe/[clotheId].ts
@@ -1,74 +1,84 @@
 import clotheRepository from '@/models/Postgre/ClotheRepositoryPostgre';
 import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
 ) {
-	let clotheId: string;
-	switch (req.method) {
-		case 'PUT':
-			clotheId = req.query.clotheId as string;
-			if (req.body.operation === 'changeCategory') {
-				const category = req.body.toUpdate.category as string;
+	const token = req.headers.authorization.split(' ')[1];
+	const userIdByToken = jwt.verify(token, process.env.JWT_SECRET);
+	if (userIdByToken) {
+		let clotheId: string;
+		switch (req.method) {
+			case 'PUT':
+				clotheId = req.query.clotheId as string;
+				if (req.body.operation === 'changeCategory') {
+					const category = req.body.toUpdate.category as string;
+					try {
+						const clothe = await clotheRepository.changeCategoryByClotheId({
+							clotheId,
+							category,
+						});
+						res.status(200).json({
+							status: 'success',
+							message: 'Propriedade category alterada com sucesso',
+							data: clothe,
+						});
+					} catch (error) {
+						res.status(400).json({
+							status: 'error',
+							message: 'Erro alterar a propriedade category',
+						});
+					}
+				}
+				if (req.body.operation === 'toggleFavorite') {
+					try {
+						const clothe = await clotheRepository.toggleFavoriteByClotheId({
+							clotheId,
+						});
+						res.status(200).json({
+							status: 'success',
+							message: 'Propriedade favorite alterada com sucesso',
+							data: clothe,
+						});
+					} catch {
+						res.status(400).json({
+							status: 'error',
+							message: 'Erro alterar a propriedade favorite',
+						});
+					}
+				}
+				break;
+			case 'DELETE':
+				clotheId = req.query.clotheId as string;
 				try {
-					const clothe = await clotheRepository.changeCategoryByClotheId({
+					const isNull = await clotheRepository.deleteByClotheId({
 						clotheId,
-						category,
 					});
+					if (isNull === null) {
+						res.status(400).json({
+							status: 'error',
+							message: 'Erro ao deletar roupa',
+						});
+						return;
+					}
 					res.status(200).json({
 						status: 'success',
-						message: 'Propriedade category alterada com sucesso',
-						data: clothe,
+						message: 'Roupa deletada com sucesso',
 					});
 				} catch (error) {
 					res.status(400).json({
 						status: 'error',
-						message: 'Erro alterar a propriedade category',
-					});
-				}
-			}
-			if (req.body.operation === 'toggleFavorite') {
-				try {
-					const clothe = await clotheRepository.toggleFavoriteByClotheId({
-						clotheId,
-					});
-					res.status(200).json({
-						status: 'success',
-						message: 'Propriedade favorite alterada com sucesso',
-						data: clothe,
-					});
-				} catch {
-					res.status(400).json({
-						status: 'error',
-						message: 'Erro alterar a propriedade favorite',
-					});
-				}
-			}
-			break;
-		case 'DELETE':
-			clotheId = req.query.clotheId as string;
-			try {
-				const isNull = await clotheRepository.deleteByClotheId({
-					clotheId,
-				});
-				if (isNull === null) {
-					res.status(400).json({
-						status: 'error',
 						message: 'Erro ao deletar roupa',
 					});
-					return;
 				}
-				res.status(200).json({
-					status: 'success',
-					message: 'Roupa deletada com sucesso',
-				});
-			} catch (error) {
-				res.status(400).json({
-					status: 'error',
-					message: 'Erro ao deletar roupa',
-				});
-			}
-			break;
+				break;
+		}
+		return;
 	}
+	res.status(400).json({
+		status: 'error',
+		message: 'Usuário não autenticado',
+	});
 }
